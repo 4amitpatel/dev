30e5be296367ec639b8af33c684189c9a3c6d6fa
4amitpatel <147070475+4amitpatel@users.noreply.github.com>
2023-10-31 10:34:59 -0400

    Update avdext1.ps1

--- avdext1.ps1
+++ avdext1.ps1
@@ -1,27 +1,14 @@
-# Define the list of extensions to install with their latest versions
-$extensions = @{
-    "AzureMonitorWindowsAgent" = (Get-AzVMExtensionImage -Location $vm.Location -PublisherName "MicrosoftMonitoringAgent" -Type "AzureMonitorWindowsAgent" | Sort-Object PublishedDate -Descending | Select-Object -First 1).Version
-    "AzurePolicyforWindows" = (Get-AzVMExtensionImage -Location $vm.Location -PublisherName "Microsoft.Policy" -Type "AzurePolicyforWindows" | Sort-Object PublishedDate -Descending | Select-Object -First 1).Version
-    "IaaSAntimalware" = (Get-AzVMExtensionImage -Location $vm.Location -PublisherName "Microsoft.Azure.Security" -Type "IaaSAntimalware" | Sort-Object PublishedDate -Descending | Select-Object -First 1).Version
-}
+# Enable Antimalware Extension
+Enable-AzureRmVMAntiMalwareExtension -ResourceGroupName "<YourResourceGroupName>" -VMName $env:COMPUTERNAME -Name "IaaSAntimalware" -Publisher "Microsoft.Azure.Security" -Type "IaaSAntimalware" -TypeHandlerVersion "1.7"
 
-# Install the extensions on the VM
-foreach ($extension in $extensions.Keys) {
-    $version = $extensions[$extension]
-
-    $extensionParams = @{
-        "VMName" = $vm.Name
-        "ResourceGroupName" = $vm.ResourceGroupName
-        "Location" = $vm.Location
-        "ExtensionName" = $extension
-        "Publisher" = "Microsoft.Compute"
-        "Type" = "CustomScriptExtension"
-        "TypeHandlerVersion" = $version
-        "Settings" = @{
-            # You will enter the custom script URL in the Azure portal during deployment
-        }
+# Configure Antimalware Extension Settings
+$antimalwareSettings = @{
+    "AntimalwareEnabled" = $true
+    "RealtimeProtectionEnabled" = $true
+    "ScheduledScanSettings" = @{
+        "isEnabled" = $true
+        "day" = 7  # Saturday
+        "time" = 120  # 2:00 AM
     }
-
-    # Install the extension on the VM
-    Set-AzVMExtension -ResourceGroupName $extensionParams.ResourceGroupName -VMName $extensionParams.VMName -Name $extensionParams.ExtensionName -Publisher $extensionParams.Publisher -ExtensionType $extensionParams.Type -TypeHandlerVersion $extensionParams.TypeHandlerVersion -Settings $extensionParams.Settings -Location $extensionParams.Location -ForceRerun -Verbose
 }
+Set-AzureRmVMAntiMalwareExtension -ResourceGroupName "<YourResourceGroupName>" -VMName $env:COMPUTERNAME -Name "IaaSAntimalware" -Publisher "Microsoft.Azure.Security" -Type "IaaSAntimalware" -TypeHandlerVersion "1.7" -Settings $antimalwareSettings
